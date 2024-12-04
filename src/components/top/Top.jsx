6174dcc9138a0b5d262dd4a6a5f2db393e47d8a2
HiteshMallick <codinglord1227@gmail.com>
2024-12-04 13:20:37 +0530

    more chnages

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -7,7 +7,7 @@ import { IoMailOutline, IoLogInOutline  } from "react-icons/io5";
 function Top() {
   return (
     <>
-    <div className='hidden lg:flex gap-3 pl-5 Top-container'>
+    <div className='hidden lg:flex gap-3 Top-container'>
       <div className='text-[#BCBCBC] contact-box'><FaWhatsapp /> <span className='pl-1'>WhatsApp Support</span></div>
       <div className='text-[#BCBCBC] contact-box'><IoMailOutline /> <span className='pl-1'>team@djfyapp.com</span></div>
       <div className='text-[#BCBCBC] contact-box'><FaPhoneAlt  /> <span className='pl-1'>+001 2345 6789 90</span></div>
@@ -21,24 +21,6 @@ function Top() {
       <div className='text-[#BCBCBC] Icon'><FaLinkedinIn /></div>
       <div className='text-[#BCBCBC] Icon'><FaInstagram /></div>
     </div>
-
-    {/* responsive */}
-
-    <div className='flex lg:hidden gap-3 pl-5 Top-container'>
-      <div className='text-[#BCBCBC] contact-box'><FaWhatsapp /></div>
-      <div className='text-[#BCBCBC] contact-box'><IoMailOutline /></div>
-      <div className='text-[#BCBCBC] contact-box'><FaPhoneAlt  /></div>
-
-      <p className='text-[#BCBCBC] login'>Login / Register</p>
-
-      {/* ICON */}
-
-      <div className='text-[#BCBCBC] Icon'><FaFacebookF /></div>
-      <div className='text-[#BCBCBC] Icon'><FaTwitter /></div>
-      <div className='text-[#BCBCBC] Icon'><FaLinkedinIn /></div>
-      <div className='text-[#BCBCBC] Icon'><FaInstagram /></div>
-    </div>
-
     </>
   )
 }
